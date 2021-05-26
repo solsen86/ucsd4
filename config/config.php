5d546fc5ad1e3c0b4037345915b2561e62d9742f
Seth Olsen <setholsen8@gmail.com>
2021-05-26 08:38:40 -0600

    created dev branch and pointed it to test db

--- config/config.php
+++ config/config.php
@@ -6,7 +6,7 @@
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'technology');
+define('DB_NAME', 'test');
 
 // Attempt to connect to MySQL Database
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
